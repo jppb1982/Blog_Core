fc4aa94b1f8cb569408b309aba7cf7e4710ebd7b
Juan Pablo Ponce <jppb1982@gmail.com>
2024-09-03 01:29:41 -0300

    Primera seccion del Proyecto terminada

---
+++ BlogCore.Modelos/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el nombre de la categoría.")]
+        [Display(Name = "Nombre de categoría")] /*Alias -> como se va a mostrar*/
+        public required string Nombre { get; set; }
+
+        [Display(Name = "Orden de la visualización")]
+
+        [Range(1, 100, ErrorMessage = "El valor debe estar entre 1 y 100")]
+        public int? Orden { get; set; }
+    }
+}
---
+++ BlogCoreAccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogCore.Modelos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Poner todos los modelos que se vayan creando
+        public DbSet<Categoria> Categoria { get; set; }
+        //public DbSet<Articulo> Articulo { get; set; }
+    }
+}
